73c627cba55100a4617d16b20c97c7d7dd768ea6
cat12079801 <cat12079801@gmail.com>
2022-12-06 22:20:15 +0900

    style: format

--- src/lib/__tests__/isbn.test.ts
+++ src/lib/__tests__/isbn.test.ts
@@ -5,8 +5,10 @@ test("ランダムなISBNを生成", () => {
 });
 
 test("ランダムな出版社記号+書籍記号を生成", () => {
-  expect(isbn.generateRandomPublisherSymbolAndBookTitleSymbol()).toMatch(/^\d{8}$/);
-})
+  expect(isbn.generateRandomPublisherSymbolAndBookTitleSymbol()).toMatch(
+    /^\d{8}$/
+  );
+});
 
 test("著名な出版社記号を選択", () => {
   expect(isbn.selectFamousPublisherSymbol()).toMatch(/^([01]\d|[2-6]\d\d)$/);

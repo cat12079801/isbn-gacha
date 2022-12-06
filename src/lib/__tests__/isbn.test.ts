b5f31d276e3521e80c41dfe5123790393fd99c59
cat12079801 <cat12079801@gmail.com>
2022-12-06 22:07:38 +0900

    test: テストコード実装

--- src/lib/__tests__/isbn.test.ts
+++ src/lib/__tests__/isbn.test.ts
@@ -1,5 +1,40 @@
 import * as isbn from "../isbn";
 
-test("空文字のISBNを生成", () => {
-  expect(isbn.generateRandomIsbn()).toBe("");
+test("ランダムなISBNを生成", () => {
+  expect(isbn.generateRandomIsbn()).toMatch(/^978-4-[\d-]{10}-\d$/);
+});
+
+test("著名な出版社記号を選択", () => {
+  expect(isbn.selectFamousPublisherSymbol()).toMatch(/^([01]\d|[2-6]\d\d)$/);
+});
+
+describe("ランダムな書名記号を生成", () => {
+  test("1桁", () => {
+    expect(isbn.generateRandomBookTitleSymbol(1)).toMatch(/^\d$/);
+  });
+  test("2桁", () => {
+    expect(isbn.generateRandomBookTitleSymbol(2)).toMatch(/^\d{2}$/);
+  });
+  test("3桁", () => {
+    expect(isbn.generateRandomBookTitleSymbol(3)).toMatch(/^\d{3}$/);
+  });
+  test("4桁", () => {
+    expect(isbn.generateRandomBookTitleSymbol(4)).toMatch(/^\d{4}$/);
+  });
+  test("5桁", () => {
+    expect(isbn.generateRandomBookTitleSymbol(5)).toMatch(/^\d{5}$/);
+  });
+  test("6桁", () => {
+    expect(isbn.generateRandomBookTitleSymbol(6)).toMatch(/^\d{6}$/);
+  });
+});
+
+describe("チェックディジットの確認", () => {
+  test("978-4-10-109205", () => {
+    expect(isbn.calculateCheckDigit("978-4-10-109205")).toBe("8");
+  });
+
+  test("000-0-00-000000", () => {
+    expect(isbn.calculateCheckDigit("000-0-00-000000")).toBe("0");
+  });
 });
--- src/lib/__tests__/openbd.test.ts
+++ src/lib/__tests__/openbd.test.ts
@@ -1,5 +1,11 @@
 import * as openbd from "../openbd";
 
-test("仮テスト", () => {
-  expect(openbd.getFromOpenbd("")).toBe("");
+describe("openBDからの情報取得", () => {
+  test("書籍1", () => {
+    expect(openbd.getFromOpenbd("9784780802047")).toBe("おにぎりレシピ101");
+  });
+
+  test("書籍2", () => {
+    expect(openbd.getFromOpenbd("9784274064531")).toBe("マスタリングTCP\\/IP");
+  });
 });

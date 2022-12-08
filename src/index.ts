0c3c9c956b90d83d94c92ed8325c03dfa10844ee
cat12079801 <cat12079801@gmail.com>
2022-12-08 23:02:15 +0900

    feat: ランダムな書籍を表示

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-const main = () => {
-  console.log("hi");
+import { getFromOpenbd } from "./lib/openbd";
+import { generateRandomIsbn } from "./lib/isbn";
+
+const main = async () => {
+  let isbn;
+  let bookTitle;
+  while (true) {
+    isbn = generateRandomIsbn();
+    bookTitle = await getFromOpenbd(isbn);
+    if (bookTitle) {
+      break;
+    }
+  }
+  console.log(`isbn: ${isbn}`);
+  console.log(`title: ${bookTitle}`);
 };
 
 main();

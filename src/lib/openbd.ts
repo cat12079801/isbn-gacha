bfddcd3c142079e73d7ea10878fc1b0eef36fa48
cat12079801 <cat12079801@gmail.com>
2022-12-08 22:53:03 +0900

    feat: openBDからのfetchを実装

--- src/lib/openbd.ts
+++ src/lib/openbd.ts
@@ -2,7 +2,14 @@
  * まずは書籍名のみを返却する
  * @param isbn
  */
-export function getFromOpenbd(isbn: string): string | null {
+export async function getFromOpenbd(isbn: string): Promise<string | null> {
   // ex. GET https://api.openbd.jp/v1/get?isbn=9784780802047
-  return "";
+  return fetch(`https://api.openbd.jp/v1/get?isbn=${isbn}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data[0] === null) {
+        return null;
+      }
+      return data[0]['summary']['title'];
+    });
 }

0ff29c171eeba9ff2d1891ed60e0f3331abb4501
cat12079801 <cat12079801@gmail.com>
2022-12-08 22:53:25 +0900

    test: テストケース修正

--- src/lib/__tests__/openbd.test.ts
+++ src/lib/__tests__/openbd.test.ts
@@ -1,11 +1,11 @@
 import * as openbd from "../openbd";
 
 describe("openBDからの情報取得", () => {
-  test("書籍1", () => {
-    expect(openbd.getFromOpenbd("9784780802047")).toBe("おにぎりレシピ101");
+  test("書籍1", async () => {
+    expect(await openbd.getFromOpenbd("9784780802047")).toBe("おにぎりレシピ101");
   });
 
-  test("書籍2", () => {
-    expect(openbd.getFromOpenbd("9784274064531")).toBe("マスタリングTCP\\/IP");
+  test("書籍2", async () => {
+    expect(await openbd.getFromOpenbd("9784274064531")).toBe("マスタリングTCP/IP");
   });
 });

1d85f6f2b0a85a79099a780c4fdf19eaeb986361
cat12079801 <cat12079801@gmail.com>
2022-12-08 22:28:53 +0900

    feat: isbn関係の関数実装

--- src/lib/isbn.ts
+++ src/lib/isbn.ts
@@ -16,14 +16,16 @@
  * 例えば「岩波書店」を表す出版社記号は「00」
  */
 export function generateRandomIsbn(): string {
-  return "";
+  const publisherAndBookTitleSymbol = generateRandomPublisherSymbolAndBookTitleSymbol();
+  const checkDigit = calculateCheckDigit(`9784${publisherAndBookTitleSymbol}`);
+  return `9784${publisherAndBookTitleSymbol}${checkDigit}`;
 }
 
 /**
  * 出版社記号と書籍記号をあわせた8桁のランダムな数値文字列を返却する
  */
 export function generateRandomPublisherSymbolAndBookTitleSymbol(): string {
-  return "";
+  return getRandomNumberString(8);
 }
 
 /**
@@ -32,13 +34,40 @@ export function generateRandomPublisherSymbolAndBookTitleSymbol(): string {
  * ref. * https://isbn.jpo.or.jp/doc/08.pdf#page=35
  */
 export function selectFamousPublisherSymbol(): string {
-  return "";
+  // TODO
+  return "00";
 }
 
 export function generateRandomBookTitleSymbol(digits: number): string {
-  return "";
+  return getRandomNumberString(digits);
 }
 
+/**
+ * 12桁の数値文字列を入力に取る
+ * @param target
+ */
 export function calculateCheckDigit(target: string): string {
-  return "";
+  let sum = 0;
+  target.split('').forEach((value, index) => {
+    if (index % 2 === 0) {
+      sum += parseInt(value) * 3;
+    } else {
+      sum += parseInt(value);
+    }
+  });
+
+  const sumLastDigit = sum % 10;
+  if (sumLastDigit === 0) {
+    return "0";
+  } else {
+    return String(10 -sumLastDigit);
+  }
+}
+
+/**
+ * 指定された桁数の0埋めされたランダムな数値文字列を返却する
+ * @param digits
+ */
+function getRandomNumberString(digits: number): string {
+  return String(Math.floor(Math.random() * (10 ** digits))).padStart(digits, '0');
 }

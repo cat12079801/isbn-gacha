6ecc477a82945d7060276cecda1f2c0ce7226709
cat12079801 <cat12079801@gmail.com>
2022-12-11 22:15:07 +0900

    feat: 一定回数ガチャに失敗で終了する

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import { getFromOpenbd } from "./lib/openbd";
 import { generateRandomIsbn } from "./lib/isbn";
 
+const MAX_RETRY_COUNT = 500;
+
 const main = async () => {
-  let isbn;
-  let bookTitle;
-  while (true) {
-    isbn = generateRandomIsbn();
-    bookTitle = await getFromOpenbd(isbn);
+  let retryCount = 0;
+
+  while (retryCount++ < MAX_RETRY_COUNT) {
+    const isbn = generateRandomIsbn();
+    const bookTitle = await getFromOpenbd(isbn);
     if (bookTitle) {
-      break;
+      console.log(`isbn: ${isbn}`);
+      console.log(`title: ${bookTitle}`);
+      return;
     }
   }
-  console.log(`isbn: ${isbn}`);
-  console.log(`title: ${bookTitle}`);
+
+  console.log("一定回数ガチャに失敗しました");
 };
 
 main();
